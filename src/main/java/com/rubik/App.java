8b764356884a58700eaae761a05bb377cd6d4598
Antoine Massias <massias.antoine.pro@gmail.com>
2025-01-16 16:10:33 +0100

    Argument management.

--- src/main/java/com/rubik/App.java
+++ src/main/java/com/rubik/App.java
@@ -18,7 +18,30 @@ public class App implements Runnable
 
 	public static void main(String[] args)
 	{
+		if (args.length == 0)
+		{
+			System.err.println("Missing arguments.");
+			System.exit(1);
+		}
+		if (args[0].equals("gui"))
+			App.startGUI(args);
+		else
+			App.startCLI(args);
+	}
+
+	private static void startGUI(String[] args)
+	{
+		if (args.length > 1)
+		{
+			System.err.println("Too many arguments.");
+			System.exit(1);
+		}
 		SwingUtilities.invokeLater(new App());
+	}
+
+	private static void startCLI(String[] args)
+	{
+		System.out.println("WIP.");
 	}
 
 }
