7c6c179ddd473ffd3aee7b8c84cdd218b59eacdc
Antoine Massias <massias.antoine.pro@gmail.com>
2025-01-17 21:42:52 +0100

    Added window icon

--- src/main/java/com/rubik/gui/Window.java
+++ src/main/java/com/rubik/gui/Window.java
@@ -1,9 +1,17 @@
 package com.rubik.gui;
 
+import java.awt.Image;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Window
 {
+	private static final String TITLE = "Rubik";
+
 	private JFrame frame;
 	private IFrameLayout layout;
 
@@ -34,7 +42,8 @@ public class Window
 
 	private void createView()
 	{
-		frame = new JFrame();
+		frame = new JFrame(TITLE);
+		this.loadIcons();
 	}
 
 	private void placeComponents()
@@ -45,6 +54,32 @@ public class Window
 
 	private void createController()
 	{
+	}
+
+	private void loadIcons()
+	{
+		final List<Image> icons = new ArrayList<>();
+		String iconPath;
+		InputStream stream;
+		ImageIcon icon;
+		byte iconData[];
+
+		for (int i = 16; i <= 256; i *= 2)
+		{
+			try
+			{
+				iconPath = "/icons/icon" + i + ".jpg";
+				stream = Window.class.getResourceAsStream(iconPath);
+				iconData = stream.readAllBytes();
+				icon = new ImageIcon(iconData);
+				icons.add(icon.getImage());
+			}
+			catch (Exception e)
+			{
+				System.err.printf("Could not load %dx%d icon\n", i, i);
+			}
+		}
+		frame.setIconImages(icons);
 	}
 
 }
