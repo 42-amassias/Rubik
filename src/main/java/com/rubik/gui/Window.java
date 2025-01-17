9a40cdafcd377152f4f2dccb9c29f8b49797a309
Antoine Massias <massias.antoine.pro@gmail.com>
2025-01-17 10:29:09 +0100

    test

--- src/main/java/com/rubik/gui/Window.java
+++ src/main/java/com/rubik/gui/Window.java
@@ -1,9 +1,19 @@
 package com.rubik.gui;
 
+import java.awt.Image;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 public class Window
 {
+	private static final String TITLE = "Rubik";
+
 	private JFrame frame;
 	private IFrameLayout layout;
 
@@ -34,7 +44,8 @@ public class Window
 
 	private void createView()
 	{
-		frame = new JFrame();
+		frame = new JFrame(TITLE);
+		this.loadIcons();
 	}
 
 	private void placeComponents()
@@ -45,6 +56,31 @@ public class Window
 
 	private void createController()
 	{
+	}
+
+	private void loadIcons()
+	{
+		try
+		{
+			final List<Image> icons = new ArrayList<>();
+			for (int i = 16; i <= 256; i *= 2)
+			{
+				System.out.print("Loading " + "/icons/icon" + i + ".jpg...");
+				final InputStream stream = Window.class.getResourceAsStream("/icons/icon" + i + ".jpg");
+				final byte iconData[] = stream.readAllBytes();
+				if (!(iconData != null && iconData.length > 0))
+					System.err.println("AAAAA");
+				final ImageIcon icon = new ImageIcon(iconData);
+				icons.add(icon.getImage());
+				System.out.println(" Done.");
+			}
+			frame.setIconImages(icons);
+		}
+		catch (Exception e)
+		{
+			System.out.println();
+			e.printStackTrace();
+		}
 	}
 
 }
